48f2b66e354eb26402f19fd555f2957735af2c21
Erik Albecker <erik.albecker@sap.com>
2025-11-05 13:32:49 +0100

    change project to flask

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from tkinter import *
+import random
+
+from flask import Flask
+
+
+def generate_hex_bin_pair(bits: int):
+    number = random.randint(0, 2**bits)
+    hex_number = hex(number)
+    bin_number = bin(number)
 
 
 if __name__ == "__main__":
-    root = Tk()
-    root.mainloop()
+    app = Flask("app")
+    app.run("0.0.0.0", debug=True, port=80)
