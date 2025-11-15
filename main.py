702c68b296c4299d95a5da1adaf8d4fc3853f630
Erik Albecker <erik.albecker@sap.com>
2025-11-15 02:36:44 +0100

    remove delete test account

--- main.py
+++ main.py
@@ -199,8 +199,5 @@ if __name__ == "__main__":
         init_json()
     with open("storage.json", "r") as file:
         JSON = load(file)
-    if JSON.get("Thomas"):
-        JSON.pop("Thomas")
-        QUEUE.put(JSON)
     Thread(target=save_to_json, daemon=True).start()
     app.run("0.0.0.0", debug=True, port=int(os.environ.get('PORT', 5000)))
